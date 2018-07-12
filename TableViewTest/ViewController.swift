cc4d375ca4431c6effe0226e195dffced66a26d2
長谷川良 <hase.industries@i.softbank.jp>
2018-07-12 23:25:35 +0900

    UI部品とコードの接続確認 Appdelegateの確認方法　command＋shift＋c ｀ViewController.swift (Interface)｀だと接続不可

--- TableViewTest/ViewController.swift
+++ TableViewTest/ViewController.swift
@@ -8,18 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate{
+    
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
+       
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
-}
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel!.text = "Row \(indexPath.row)"
+        return cell
+    }
+    }
 
